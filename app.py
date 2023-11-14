ad00dbdfde5a403552cd9c51aa8060044481bb06
Kathan <shahkathan50@gmail.com>
2023-11-14 17:50:24 +0530

    modified some templates, app.py, database.py and added a page to apply for job

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
   jobs = load_jobs_from_db()
-  return render_template("home.html", jobs=jobs, company_name="KS")
+  return render_template("home.html", jobs=jobs)
 
 # This will return list of jobs data not in form of HTML but in form of json.
 @app.route("/api/jobs")
@@ -14,6 +14,19 @@ def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
 
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+  else:
+    return render_template('jobpage.html', job=job)
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  # data = request.args # request.args get the data from the url when the data is sent in the url without using post method.
+  data = request.form   # request.form will get data from when it is sended using post method.
+  return jsonify(data)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
 import os
 
-db_connection_url = os.environ['DATABASE_CONNECTION_URL_STRING']
-engine = create_engine(db_connection_url)
+load_dotenv()
+
+db_connection_url = os.environ.get('DATABASE_CONNECTION_URL_STRING')
+engine = create_engine(db_connection_url) 
 
 def load_jobs_from_db():
   with engine.connect() as conn:
@@ -11,3 +14,15 @@ def load_jobs_from_db():
     for row in result.all():
         jobs.append(row._asdict())
     return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+
+    result = conn.execute(text("select * from jobs where id = {}".format(id)))
+
+    rows = result.all()
+    print(rows)
+    if len(rows) == 0:
+      return None
+    else:
+      return rows[0]._asdict()
